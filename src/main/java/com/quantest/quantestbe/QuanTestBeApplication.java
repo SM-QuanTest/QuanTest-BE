538014b916d1ff6cd8ae25037e6a7723f0c1da1c
sujeongmoon <msjk9000@gmail.com>
2025-07-11 23:18:15 +0900

    add

---
+++ src/main/java/com/quantest/quantestbe/QuanTestBeApplication.java
@@ -0,0 +1,13 @@
+package com.quantest.quantestbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanTestBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanTestBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantest/quantestbe/QuanTestBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantest.quantestbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanTestBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
